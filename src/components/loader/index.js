f16fce26b76aaa19d7550256b1ceaec47a41db54
iappaPedroza <itstecnologia.criativa@gmail.com>
2022-01-14 20:24:12 -0300

    Projeto Dio GitHub Api - Loader componentizado

---
+++ src/components/loader/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+const Loader = () => {
+  return (
+    <S.Wrapper>
+      <h1>Loading...</h1>
+    </S.Wrapper>
+  );
+};
+
+export default Loader
---
+++ src/components/loader/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin: 16px;
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    font-weight: bold;
+    font-size: 32px;
+    color: blue;
+  }
+`;
+export const WrapperImage = styled.img`
+  width: 30px;
+  margin: 8px;
+`;
